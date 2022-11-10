0454a22a6ca257237764b0c78afc5192f0e89a31
ReicaD <dbamanya447@gmail.com>
2022-11-10 16:07:23 -0500

    upgraded forms by reducing the code size popular.js

--- app/components/data/data.js
+++ app/components/data/data.js
@@ -1,29 +1,74 @@
-const socialReviews = [
+export const socialReviews = [
   {
-    name: 'Guy Hawkins',
+    name: "Guy Hawkins",
     review:
-      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore.',
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore.",
   },
   {
-    name: 'Esther Howard',
+    name: "Esther Howard",
     review:
-      '  adipisci velit, sed quia non numquam eius modi tempora incidunt ut',
+      "  adipisci velit, sed quia non numquam eius modi tempora incidunt ut",
   },
   {
-    name: 'Amina',
+    name: "Amina",
     review:
-      ' dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est',
+      " dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est",
   },
   {
-    name: 'Musa',
+    name: "Musa",
     review:
-      '  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil,',
+      "  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil,",
   },
   {
-    name: 'Ali',
+    name: "Ali",
     review:
-      '  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil,',
+      "  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil,",
   },
 ];
+//created data to be used by the popular components infom of an array objects
+export const DietGuru = [
+  {
+    nameDiet: "Slim",
+    kcalI: 1000,
+    kcalII: 2000,
+    kcalIII: 1500,
+    description:
+      "Some people have used this time to start new exercise.Here are the different types of Slim diet meals Click the button BELOW ",
+    image: "meal11.png",
+    price: 9.99,
+  },
+  {
+    nameDiet: "Fit",
+    kcalI: 1400,
+    kcalII: 1300,
+    kcalIII: 1100,
+    description:
+      "Movement is more important now than ever.Here are the different types of Fit diet meals Click the button BELOW ",
+    image: "meal2.png",
+    price: 7.99,
+  },
+  {
+    nameDiet: "Vegan",
+    kcal1: 1600,
+    kcal2: 1500,
+    kcal3: 1505,
+    description:
+      "Let’s look at vegans versus a standard American diet.Here are the different types of Vegan diet meals Click the button BELOW",
+    image: "meal5.png",
+    price: 8.99,
+  },
 
-export default socialReviews;
+  {
+    nameDiet: "Keto",
+    kcal4: 1820,
+    kcal6: 1325,
+    kcal8: 1780,
+    description:"When it comes to diets, most studies examine population cohorts using food frequency questionnaires.Types of Keto diet meals Click the button BELOW",
+    image: "meal4.png",
+    price: 9.99
+
+  },
+
+  
+];
+
--- app/pages/Home/components/Popular.js
+++ app/pages/Home/components/Popular.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
-import NextImage from 'next/image';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import NextImage from "next/image";
+import { DietGuru } from "../../../components/data/data";
+// console.log(DietGuru);
+import { Button, Stack } from "@chakra-ui/react";
+
+// setup data file
+// export it then use it inside component
+// set it in state
 
 const Container = styled.div`
   margin-top: 50px;
@@ -89,92 +96,50 @@ const Value = styled.div`
     font-weight: bold;
   }
 `;
-
+//below we set the state for the data.js data to be displayed by using react hooks{useState,useEffect}
 const Popular = () => {
+  const [diet, setDiet] = useState([]);
+
+  useEffect(() => {
+    setDiet(DietGuru);
+
+    console.log("", diet);
+  }, []);
+  //by returning we mapo all the data inside data.js by using one parenthesis that calls for all the data one by one.
   return (
     <Container>
-      <h1 style={{ fontWeight: '600', fontSize: '30px' }}>
+      <h1 style={{ fontWeight: "600", fontSize: "30px" }}>
         The most popular diets
       </h1>
       <Wrapper>
-        <Card>
-          <img src='/meal11.png' alt='#' />
-          <h3>Slim</h3>
-          <Benefits>
-            <p>1000 kcal</p>
-            <p>2000 kcal</p>
-            <p>1500 kcal</p>
-          </Benefits>
-          <p style={{ height: '80px' }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.{' '}
-          </p>
-          <Value>
-            <p>
-              from <span>$9.99</span>/day
-            </p>
-            <button>Choose</button>
-          </Value>
-        </Card>
-
-        <Card>
-          <img src='/meal2.png' alt='#' />
-          <h3>Fit</h3>
-          <Benefits>
-            <p>1400 kcal</p>
-            <p>1300 kcal</p>
-            <p>1100 kcal</p>
-          </Benefits>
-          <p style={{ height: '80px' }}>
-            adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-          </p>
-          <Value>
-            {' '}
-            <p>
-              from <span>$7.99</span>/day
-            </p>
-            <button>Choose</button>
-          </Value>
-        </Card>
-        <Card>
-          <img src='/meal5.png' alt='#' />
-          <h3>Vegan</h3>
-          <Benefits>
-            <p>1600 kcal</p>
-            <p>1500 kcal</p>
-            <p>1505 kcal</p>
-          </Benefits>
-          <p style={{ height: '80px' }}>
-            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
-            quisquam est
-          </p>
-          <Value>
-            {' '}
-            <p>
-              from <span>$8.99</span>/day
-            </p>
-            <button>Choose</button>
-          </Value>
-        </Card>
-        <Card>
-          <img src='/meal4.png' alt='#' />
-          <h3>Keto</h3>
-          <Benefits>
-            <p>1820 kcal</p>
-            <p>1325 kcal</p>
-            <p>1780 kcal</p>
-          </Benefits>
-          <p style={{ height: '80px' }}>
-            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-            quam nihil,
-          </p>
-          <Value>
-            <p>
-              from <span>$9.99</span>/day
-            </p>
-            <button>Choose</button>
-          </Value>
-        </Card>
+        {diet.map((x) => {
+          return (
+            <Card key={x.id}>
+              <img src={x.image} alt="#" />
+              <h3>{x.nameDiet}</h3>
+              <Benefits>
+                <p>1000 kcal</p>
+                <p>2000 kcal</p>
+                <p>1500 kcal</p>
+              </Benefits>
+              <p style={{ height: "80px" }}>{x.description} </p>
+              <br></br>
+              <Value>
+                <p>
+                  from <span>${x.price}</span>/day
+                </p>
+            
+            {/* //buton was fetched from imported library (chakra) */}
+                <Stack direction="row" spacing={2}>
+                  <button>Choose</button>
+                  <Button colorScheme="teal" variant="solid">
+                    Email
+                  </Button>
+                </Stack>
+              </Value>
+            </Card>
+          );
+        })}
       </Wrapper>
     </Container>
   );
